b166a09f363542b0c267adb97f9aaff01ca98e6b
habioye <78454514+habioye@users.noreply.github.com>
2024-06-19 06:41:13 -0500

    Added the authentification request information for the database

--- src/main/java/Hassan/TaskManager/TaskManager/Entity/AuthRequest.java
+++ src/main/java/Hassan/TaskManager/TaskManager/Entity/AuthRequest.java
@@ -1,2 +1,15 @@
-package Hassan.TaskManager.TaskManager.Entity;public class AuthRequest {
+package Hassan.TaskManager.TaskManager.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
 }
