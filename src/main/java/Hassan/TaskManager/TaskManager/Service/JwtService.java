1c026877ce433ce07d87e2bec00f647fdbc2dfc5
habioye <78454514+habioye@users.noreply.github.com>
2024-06-19 08:08:43 -0500

    Deleted redundant classes and added the secreteKey and expiration time using application properties

--- src/main/java/Hassan/TaskManager/TaskManager/Service/JwtService.java
+++ src/main/java/Hassan/TaskManager/TaskManager/Service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +17,11 @@ import java.util.function.Function;
 
 @Component
 public class JwtService {
-
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    @Value("${security.jwt.secret-key}")
+    private String SECRET;
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+//    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
@@ -28,7 +32,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
