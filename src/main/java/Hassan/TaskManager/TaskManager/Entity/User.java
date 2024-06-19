b01382def57b2c9c0764ac72e9271168f6497ad1
habioye <78454514+habioye@users.noreply.github.com>
2024-06-19 06:41:56 -0500

    Added the user information for the database

--- src/main/java/Hassan/TaskManager/TaskManager/Entity/User.java
+++ src/main/java/Hassan/TaskManager/TaskManager/Entity/User.java
@@ -1,2 +1,55 @@
-package Hassan.TaskManager.TaskManager.Entity;public class User {
+package Hassan.TaskManager.TaskManager.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String name;
+
+    private String email;
+    private String password;
+    private String roles;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
