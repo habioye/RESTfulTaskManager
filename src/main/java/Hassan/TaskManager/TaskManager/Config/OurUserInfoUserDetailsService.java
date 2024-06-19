712205ad5080aa6e07156de72c1f3cc125bdf575
habioye <78454514+habioye@users.noreply.github.com>
2024-06-19 06:40:36 -0500

    Updated the OurUser class to User

--- src/main/java/Hassan/TaskManager/TaskManager/Config/OurUserInfoUserDetailsService.java
+++ src/main/java/Hassan/TaskManager/TaskManager/Config/OurUserInfoUserDetailsService.java
@@ -1,7 +1,7 @@
 package Hassan.TaskManager.TaskManager.Config;
 
-import com.springSecurityUpdated.springSecurityUpdated.model.OurUser;
-import com.springSecurityUpdated.springSecurityUpdated.repository.OurUserRepo;
+import Hassan.TaskManager.TaskManager.Entity.User;
+import Hassan.TaskManager.TaskManager.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,10 +13,10 @@ import java.util.Optional;
 @Configuration
 public class OurUserInfoUserDetailsService implements UserDetailsService {
     @Autowired
-    private OurUserRepo ourUserRepo;
+    private UserRepository ourUserRepo;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<OurUser> user = ourUserRepo.findByEmail(username);
+        Optional<User> user = ourUserRepo.findByName(username);
         return user.map(OurUserInfoDetails::new).orElseThrow(()->new UsernameNotFoundException("User Does Not Exist"));
     }
 }
